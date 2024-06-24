405d151c32ea4f94698b91f4dd216e4da85174d2
Biswajit Ghosal <biswajit.ghosal@exavalu.com>
2024-06-24 21:31:26 +0530

    added new features

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, jsonify, request, render_template
-from readEmail import read_email_data
-from openaiRAG import generate_content_from_documents
 import os
 from dotenv import load_dotenv
+from flask_cors import CORS
+# Folder Modules
+from readEmail import read_email_data
+from openaiRAG import generate_content_from_documents
+from aws_s3 import list_folders, list_files_in_folder
 
 load_dotenv()
 host = os.getenv("host")
 app = Flask(__name__)
+CORS(app)
 
 
 # Route for the home page
@@ -37,6 +41,33 @@ def generate_data():
         return response, 200
     except Exception as e:
         return jsonify({"message": f"Unable to generate response: {str(e)}"}), 500
+
+
+@app.route('/api/submission_list', methods=['GET'])
+def list_submission():
+    try:
+        folders = list_folders()
+        if len(folders) == 0:
+            return jsonify({"message": "No Submission Available"}), 200
+        else:
+            return folders, 200
+    except:
+        return jsonify({"message": "Unable to connect to server"}), 500
+
+
+@app.route('/api/files_list', methods=['GET'])
+def list_files():
+    try:
+        submission_id = request.args.get("submission_id")
+        if not submission_id:
+            return jsonify({"message": "Please Provide Submission Id"}), 400
+        folders = list_files_in_folder(submission_id=submission_id)
+        if len(folders) == 0:
+            return jsonify({"message": "No Submission Available"}), 200
+        else:
+            return folders, 200
+    except:
+        return jsonify({"message": "Unable to connect to server"}), 500
 
 
 if __name__ == '__main__':
--- aws_s3.py
+++ aws_s3.py
@@ -4,31 +4,60 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_KEY = os.getenv("API_KEY")
-AWS_UNDERWRITER_BUCKET = os.getenv("AWS_UNDERWRITER_BUCKET")
-AWS_ACCESS_KEY_ID = os.getenv("aws_access_key_id")
-AWS_SECRET_ACCESS_KEY = os.getenv("aws_secret_access_key")
-# AWS_SESSION_TOKEN = os.getenv("AWS_SESSION_TOKEN")
+# Retrieve AWS credentials and region from environment variables
+aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+aws_region = os.getenv('AWS_REGION')
+bucket_name = os.getenv('AWS_UNDERWRITER_BUCKET')
 
-s3_client = boto3.client(
-    "s3",
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY
-    # aws_session_token=AWS_SESSION_TOKEN,
+# Create a session using your AWS credentials
+session = boto3.Session(
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
+    region_name=aws_region
 )
+s3_client = session.client('s3')
 
 
 def upload_file_to_s3(submission_id=None, filename=None, content=None):
     try:
         # file = f"./static/uploads/{submission_id}_input.xlsx"
         # s3_client.upload_file(file, AWS_UNDERWRITER_BUCKET, f'{submission_id}_input.xlsx')
-        response = s3_client.put_object(Bucket=AWS_UNDERWRITER_BUCKET, Key=f"input/{submission_id}/{filename}",
+        response = s3_client.put_object(Bucket=bucket_name, Key=f"input/{submission_id}/{filename}",
                                         Body=content)
         status = response.get("ResponseMetadata", {}).get("HTTPStatusCode")
-        url = f"https://{AWS_UNDERWRITER_BUCKET}.s3.amazonaws.com/{submission_id}/{filename}"
+        url = f"https://{bucket_name}.s3.amazonaws.com/{submission_id}/{filename}"
         if status == 200:
             return url
         return None
     except Exception as e:
         print(e)
         return False
+
+
+def list_folders():
+    # List objects in the bucket
+    response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix='input/', Delimiter='/')
+    # Check if the response contains CommonPrefixes which denotes the folders
+    if 'CommonPrefixes' in response:
+        folders = [prefix['Prefix'] for prefix in response['CommonPrefixes']]
+        # Remove the 'input/' prefix from folder names
+        folders = [folder.replace('input/', '').replace('/', '') for folder in folders]
+        return folders
+    else:
+        return []
+
+
+def list_files_in_folder(submission_id=None):
+    # List objects in the specified folder within the "input" directory of the bucket
+    prefix = f'input/{submission_id}/'
+    response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+
+    # Check if the response contains Contents which denotes the files
+    if 'Contents' in response:
+        files = [content['Key'] for content in response['Contents']]
+        # Remove the 'input/{folder_name}/' prefix from file names
+        files = [file.replace(prefix, '') for file in files]
+        return files
+    else:
+        return []
